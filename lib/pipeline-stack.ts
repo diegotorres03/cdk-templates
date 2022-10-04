d822884f7fe4f3e598da390d9f81b25441f2bc77
Diego Torres <diegotorres0303@gmail.com>
2022-10-04 16:38:24 -0500

    looking at pipelines too

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,18 +1,20 @@
-import { 
-    pipelines,
-    Stack, StackProps, 
-} from "aws-cdk-lib"
-import { Construct } from "constructs"
+// import { 
+//     pipelines,
+//     Stack, StackProps, 
+// } from "aws-cdk-lib"
+// import { Construct } from "constructs"
 
 
 
-export class PipelineStack extends Stack {
-    constructor(scope: Construct, id:string, props?: StackProps) {
-        super(scope, id, props)
+// export class PipelineStack extends Stack {
+//     constructor(scope: Construct, id:string, props?: StackProps) {
+//         super(scope, id, props)
 
-        const pipeline = new 
+//         const pipeline = new pipelines.CodePipeline(this, 'pipeline', {
+//             synth
+//         }) 
 
 
-    }    
-}
+//     }    
+// }
 
