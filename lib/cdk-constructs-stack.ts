64762ff8d6b2ab9c1a50635351088cd207e22dc1
Diego Torres <diegotorres0303@gmail.com>
2022-11-23 09:48:36 -0600

    testing build step

--- lib/cdk-constructs-stack.ts
+++ lib/cdk-constructs-stack.ts
@@ -43,7 +43,7 @@ export class CdkConstructsStack extends cdk.Stack {
       })
       .build({
         version: '0.2',
-        pahses: {
+        phases: {
           pre_build: {
             commands: [
               `aws codeartifact login --tool npm --domain ${artifactDomain.domainName} --repository ${artifactRepo.repositoryName}`
