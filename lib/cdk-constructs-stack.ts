ae33f97665db348cc98564bbc0251103c9f069b5
Diego Torres <diegotorres0303@gmail.com>
2022-11-22 19:15:57 -0600

    testing build step

--- lib/cdk-constructs-stack.ts
+++ lib/cdk-constructs-stack.ts
@@ -38,7 +38,7 @@ export class CdkConstructsStack extends cdk.Stack {
           },
           pre_build: {commands: ['npm --version', 'node --version']},
           build: { commands: ['cdk deploy --all'] },
-          post_build: {commands: ['echo "done"']},
+          post_build: {commands: ['echo "done with deployment"']},
         }
       })
 
