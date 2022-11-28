d9beaf0fc081435fb0c4f4e1e994d8f9759009ab
Diego Torres <diegotorres0303@gmail.com>
2022-11-28 11:44:12 -0600

    initial commit

---
+++ update-versions.ps1
@@ -0,0 +1,17 @@
+Set-Location lib/dynamodb
+npm version patch
+
+Set-Location ../graphql
+npm version patch
+
+Set-Location ../rest-api
+npm version patch
+
+Set-Location ../webapp
+npm version patch
+
+
+Set-Location ../pipeline
+npm version patch
+
+Set-Location ../..
